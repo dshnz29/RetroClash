15e90480c64a523a9c42f44f8b615f4230bb9ee2
Inuka-G <156221307+Inuka-G@users.noreply.github.com>
2025-08-13 16:18:28 +0530

    add src copy

---
+++ client/src copy/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navbar from "@/components/navbar"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "RETRO CLASH",
+  description: "A gaming website for physical Pong game",
+  generator: "",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
